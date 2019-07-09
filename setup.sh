32617182e2628d065b1dd96cedf4138bee4dfa6e
Tiago Nascimento <tiago@theosirian.com>
2019-07-09 06:33:07 -0300

    xsession added

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-for i in zshrc bashrc vimrc xinitrc Xresources tmux.conf urxvt gdbinit; do
+for i in zshrc bashrc vimrc xinitrc Xresources tmux.conf urxvt gdbinit xsession; do
    ln -s "$HOME/$1/$i" "$HOME/.$i"
 done
